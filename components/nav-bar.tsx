27ba337a5aa751d08fe3f15d788d1015d8f5d8fd
YDG <zzmn1234@naver.com>
2025-02-10 18:45:54 +0900

    feat: pretend font 추가

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -4,16 +4,16 @@ import React from 'react';
 export function NavBar() {
   return (
     <div className=' flex justify-around '>
-      <Link href='/' className='py-2 px-4'>
+      <Link href='/' className='py-2 px-4 text-white'>
         Home
       </Link>
-      <Link href='/posts' className='py-2 px-4'>
+      <Link href='/posts' className='py-2 px-4 text-white'>
         Posts
       </Link>
-      <Link href='/about' className='py-2 px-4'>
+      <Link href='/about' className='py-2 px-4 text-white'>
         About
       </Link>
-      <Link href='/guest' className='py-2 px-4'>
+      <Link href='/guest' className='py-2 px-4 text-white'>
         Guest
       </Link>
     </div>
