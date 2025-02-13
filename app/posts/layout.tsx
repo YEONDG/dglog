c015d7ae6fbf82e30df97fecf74b79d60935acf3
YDG <zzmn1234@naver.com>
2025-02-13 23:27:04 +0900

    remove: 사이드바 제거

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -1,23 +1,3 @@
-import { cookies } from 'next/headers';
-
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
-import { AppSidebar } from '@/components/app-sidebar';
-
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const cookieStore = await cookies();
-  const defaultOpen = cookieStore.get('sidebar_state')?.value === 'true';
-
-  return (
-    <SidebarProvider defaultOpen={defaultOpen} className=''>
-      <AppSidebar />
-      <main className='w-full'>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
-  );
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  return <main className='w-full'>{children}</main>;
 }
