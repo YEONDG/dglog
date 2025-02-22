347abc60ee58c9851e7bbfbe61c46b47822ba056
YDG GBpro5 <zzmn1234@naver.com>
2025-02-22 19:16:15 +0900

    feat: guestbook password 스키마 추가가

---
+++ schemas/guestbook.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const guestbookSchema = z.object({
+  name: z.string().min(1, '이름은 필수입니다.').max(10, '이름은 10자 이하여야 합니다.'),
+  message: z.string().min(1, '메시지는 필수입니다.').max(30, '메시지는 30자 이하여야 합니다.'),
+  password: z.string().min(1, '비밀번호는 필수입니다.').max(10, '비밀번호는 10자 이하여야 합니다.'),
+});
+
+export type GuestbookSchema = z.infer<typeof guestbookSchema>;
