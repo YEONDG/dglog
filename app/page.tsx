461c9e88880f4f0b6cbe7788d2a54d897e2d0b60
YDG GBpro5 <zzmn1234@naver.com>
2025-02-24 21:23:58 +0900

    wip: home 프로필만드는중중

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image';
+
 const Home = () => {
   return (
     <div>
-      <h1></h1>
+      <div className='relative w-40 h-52'>
+        <Image
+          src={'/연동근.jpg'}
+          alt='profile image'
+          fill
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+          style={{ objectFit: 'cover' }}
+          priority
+        />
+      </div>
+      <h1>신입 프론트엔드 개발자 연동근입니다.</h1>
     </div>
   );
 };
