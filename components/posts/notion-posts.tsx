3bde0752d10d1cb60c5b03424e2973061bb397d8
YDG <zzmn1234@naver.com>
2025-02-28 21:40:27 +0900

    refactor: 포스트 컴포넌트 구조 개선

---
+++ components/posts/notion-posts.tsx
@@ -0,0 +1,41 @@
+import { getNotionPosts } from '@/lib/notion';
+import Link from 'next/link';
+import React from 'react';
+import { formatDate } from '@/lib/utils';
+
+export const dynamic = 'force-static';
+
+export const NotionPosts = async () => {
+  const posts = await getNotionPosts();
+  return (
+    <>
+      {posts.map((post) => (
+        <li
+          key={post.id}
+          className='flex flex-col justify-start border-2 hover:shadow-lg transition-shadow duration-300 py-1 px-2 rounded-md h-16'
+        >
+          {/* 구조 변경: 중첩된 링크 제거 */}
+          <div className='flex justify-between items-center'>
+            <Link href={`/posts/${post.id}`} className='text-lg hover:underline'>
+              {post.properties.제목.title[0]?.plain_text || '이름 없음'}
+            </Link>
+            <div className='text-sm text-gray-700'>{formatDate(post.properties.생성일.created_time)}</div>
+          </div>
+
+          {/* 태그 섹션을 별도 행으로 분리 */}
+          <div className='hidden md:flex gap-1 mt-1 justify-end'>
+            {post.properties.태그.multi_select.map((tag) => (
+              <Link
+                key={tag.name}
+                href={`/posts?tag=${tag.name}`}
+                className='px-2 py-1 text-xs rounded-md hover:bg-gray-100'
+              >
+                {tag.name}
+              </Link>
+            ))}
+          </div>
+        </li>
+      ))}
+    </>
+  );
+};
