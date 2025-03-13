07e95d6de57c27680b85f74d7491f181a4b35727
YDG <zzmn1234@naver.com>
2025-03-13 21:55:32 +0900

    fix: Google Analytics useSearchParams 제거하여 빌드 오류 수정

--- components/google-analytics.jsx
+++ components/google-analytics.jsx
@@ -1,23 +1,19 @@
 'use client';
 
 import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 
 export default function GoogleAnalytics({ GA_MEASUREMENT_ID }) {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
-    if (GA_MEASUREMENT_ID) {
-      // 페이지 URL 구성 (경로 + 쿼리 파라미터)
-      const url = pathname + (searchParams?.toString() ? `?${searchParams.toString()}` : '');
-
-      // GA 페이지뷰 이벤트 전송
+    if (GA_MEASUREMENT_ID && pathname) {
+      // GA 페이지뷰 이벤트 전송 (쿼리 파라미터 없이)
       window.gtag('config', GA_MEASUREMENT_ID, {
-        page_path: url,
+        page_path: pathname,
       });
     }
-  }, [pathname, searchParams, GA_MEASUREMENT_ID]);
+  }, [pathname, GA_MEASUREMENT_ID]);
 
   return (
     <>
