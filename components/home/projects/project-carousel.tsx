fc84660fd1ce97fd3f705162897b03f5de3db629
YDG <zzmn1234@naver.com>
2025-03-27 19:05:09 +0900

    feat: shadcn-carousel 추가

---
+++ components/home/projects/project-carousel.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import { Card, CardContent } from '@/components/ui/card';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+import Image from 'next/image';
+
+export function ProjectCarousel() {
+  return (
+    <Carousel className='w-full'>
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className='p-1'>
+              <Card>
+                <CardContent className='relative flex aspect-video items-center justify-center p-6'>
+                  <Image
+                    src={'/project/큐트채팅이미지.webp'}
+                    fill
+                    alt={`${index}`}
+                    className='rounded-xl object-fill'
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
