cc67d4feb467859c6d8013c462b087fe5856beca
YDG <zzmn1234@naver.com>
2025-04-03 22:37:09 +0900

    feat: 큐트채팅 이미지 추가

--- components/home/projects/cutechatting.tsx
+++ components/home/projects/cutechatting.tsx
@@ -11,7 +11,7 @@ const CuteChatting = () => {
       </h1>
       <div className="w-full md:px-6">
         <section className="flex justify-center">
-          <ProjectCarousel />
+          <ProjectCarousel projectName="cutechatting" />
         </section>
       </div>
       <div className="grid px-1 py-4 md:grid-flow-col md:px-4">
