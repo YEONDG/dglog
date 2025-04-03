25ec0d65eec05d661c54504edda5f75e8aeeb0af
YDG <zzmn1234@naver.com>
2025-04-03 22:41:50 +0900

    feat: project 이미지 array 관리

--- components/home/projects/project-carousel.tsx
+++ components/home/projects/project-carousel.tsx
@@ -10,19 +10,56 @@ import {
 } from "@/components/ui/carousel";
 import Image from "next/image";
 
-export function ProjectCarousel() {
+type ProjectName = "dglog" | "cutechatting" | "pokemon" | "image-app";
+
+type ProjectImagesMap = {
+  [key in ProjectName]: string[];
+};
+
+const projectImagesData: ProjectImagesMap = {
+  cutechatting: [
+    "/project-img/cutechatting-1webp.webp",
+    "/project-img/cutechatting-2webp.webp",
+    "/project-img/cutechatting-3webp.webp",
+    "/project-img/cutechatting-4webp.webp",
+  ],
+  dglog: [
+    "/project-img/dglog-1webp.webp",
+    "/project-img/dglog-2webp.webp",
+    "/project-img/dglog-3webp.webp",
+    "/project-img/dglog-4webp.webp",
+  ],
+  pokemon: [
+    "/project-img/pokemon-1webp.webp",
+    "/project-img/pokemon-2webp.webp",
+    "/project-img/pokemon-3webp.webp",
+    "/project-img/pokemon-4webp.webp",
+  ],
+  "image-app": [
+    "/project-img/image-app-1webp.webp",
+    "/project-img/image-app-2webp.webp",
+  ],
+};
+
+interface ProjectCarouselProps {
+  projectName: ProjectName;
+}
+
+export function ProjectCarousel({ projectName }: ProjectCarouselProps) {
+  const images = projectImagesData[projectName];
+
   return (
     <Carousel className="w-full max-w-lg">
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
+        {images.map((imageUrl, index) => (
           <CarouselItem key={index}>
             <div className="p-1">
               <Card>
                 <CardContent className="relative flex aspect-video items-center justify-center p-6">
                   <Image
-                    src={"/project-img/큐트채팅이미지.webp"}
+                    src={imageUrl}
                     fill
-                    alt={`${index}`}
+                    alt={`${projectName} image ${index + 1}`}
                     className="rounded-xl object-fill"
                   />
                 </CardContent>
