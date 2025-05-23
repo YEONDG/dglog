19d93a6ea5cb3e424ff4cbbe1697743b6feff176
YDG <zzmn1234@naver.com>
2025-05-23 23:39:02 +0900

    refactor: update rotation interval in HeroHeader component for improved animation timing

--- components/home/hero-header.tsx
+++ components/home/hero-header.tsx
@@ -26,7 +26,7 @@ export const HeroHeader = () => {
               staggerDuration={0.025}
               splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
               transition={{ type: "spring", damping: 30, stiffness: 400 }}
-              rotationInterval={3000}
+              rotationInterval={2000}
             />
             <span className="text-6xl font-bold">좋아하는</span>
           </div>
